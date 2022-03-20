d24b32741ff5bfc39ac0679c1bf5ee60278787b7
Waihon Yew <yewwaihon@gmail.com>
2022-03-20 17:24:31 +0800

    Create the routes to post, put, and delete reviews

--- api/movies.route.js
+++ api/movies.route.js
@@ -1,8 +1,13 @@
 import express from 'express';
 import MoviesController from './movies.controller.js';
+import ReviewsController from './reviews.controller.js';
 
 const router = express.Router();
 
 router.route('/').get(MoviesController.apiGetMovies);
+router.route('/review')
+  .post(ReviewsController.apiPostReview)
+  .put(ReviewsController.apiPutReview)
+  .delete(ReviewsController.apiDeleteReview);
 
 export default router;
